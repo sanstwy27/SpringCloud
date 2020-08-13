a9c198c96bc38a762af915f04aca272372a64db4
sanstwy777 <sanstwy777@outlook.com>
2020-08-13 14:57:31 +0800

    practice SpringCloud-Config

---
+++ cloud-config-center-3344/src/main/java/com/atsanstwy27/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.atsanstwy27.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Sanstwy27
+ * @create 8/13/2020
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/atsanstwy27/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.atsanstwy27.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Sanstwy27
+ * @create 8/13/2020
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
