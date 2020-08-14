acd3d6c94695f05c4f5b319e852e3da622e382af
sanstwy777 <sanstwy777@outlook.com>
2020-08-14 08:52:13 +0800

    practice SpringCloud-Bus

---
+++ cloud-config-client-3366/src/main/java/com/atsanstwy27/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.atsanstwy27.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Sanstwy27
+ * @create 8/14/2020
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
